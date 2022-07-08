d95c0492cf40c514bae712e272da9d7e56c133f0
nikolapivac <npivac00@fesb.hr>
2022-07-08 12:35:48 +0200

    Added simple UI

--- script.js
+++ script.js
@@ -33,17 +33,61 @@ function singleRound(playerSelection, computerSelection){
 
 }
 
-function game(){
-    for(let i = 0; i<5; i++){
-        playerSelection = prompt(`ROUND ${i+1} - Choose: rock, paper or scissors?`);
-        computerSelection = computerPlay();
-        console.log(singleRound(playerSelection, computerSelection));
-        console.log(`Current score:\nComputer - ${cPoints}\nYou - ${pPoints}`)
+function printResult(){
+    if(cPoints == 5 && pPoints == 5){
+        results.textContent = `Results: Computer - ${cPoints} /// Player - ${pPoints} 
+        /// It's a tie!`;
+        cPoints=0;
+        pPoints=0;
     }
-    if(cPoints > pPoints){
-        console.log("You lost!")
+    else if (pPoints == 5){
+        results.textContent = `Results: Computer - ${cPoints} /// Player - ${pPoints}
+        /// The player won! :)`;
+        cPoints=0;
+        pPoints=0;
     }
-    else console.log("You won!")
+    else if (cPoints == 5){
+        results.textContent = `Results: Computer - ${cPoints} /// Player - ${pPoints}
+        /// The computer won! :(`;
+        cPoints=0;
+        pPoints=0;
+    }
+    else{
+        results.textContent = `Results: Computer - ${cPoints} /// Player - ${pPoints}`
+    }
 }
 
-game();
+const container = document.querySelector('.container')
+
+const results = document.createElement('div');
+results.classList.add('results');
+results.textContent = `Results: Computer - ${cPoints} /// Player - ${pPoints}`;
+
+const rockBtn = document.createElement('button');
+rockBtn.classList.add('rock');
+rockBtn.textContent = "ROCK";
+rockBtn.addEventListener("click", () => {
+    console.log(singleRound('rock', computerPlay()));
+    printResult();
+});
+
+const paperBtn = document.createElement('button');
+paperBtn.classList.add('paper');
+paperBtn.textContent = "PAPER";
+paperBtn.addEventListener("click", () => {
+    console.log(singleRound('paper', computerPlay()));
+    printResult();
+});
+
+const scissorsBtn = document.createElement('button');
+scissorsBtn.classList.add('scissors');
+scissorsBtn.textContent = "SCISSORS";
+scissorsBtn.addEventListener("click", () => {
+    console.log(singleRound('scissors', computerPlay()));
+    printResult();
+});
+
+container.appendChild(rockBtn);
+container.appendChild(paperBtn);
+container.appendChild(scissorsBtn);
+container.appendChild(results);
